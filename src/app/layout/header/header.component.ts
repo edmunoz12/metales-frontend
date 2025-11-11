d4315dbbbe250d0bc349675ce907c2923f2f7c9c
Edgar Muñoz <edmunoz@parpro.com>
2025-11-10 16:53:50 -0800

    header update

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,14 +1,41 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService, User } from '../../services/auth.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  user: User | null = null;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    // Escucha cambios del usuario actual
+    this.authService.currentUser$.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  logout() {
+    this.authService.logout().subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.error('Error al cerrar sesión:', err);
+        // Aunque falle el backend, limpiamos el front:
+        localStorage.removeItem('user');
+        this.router.navigate(['/login']);
+      }
+    });
+  }
 
 }
