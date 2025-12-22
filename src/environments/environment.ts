1bb82768e7c60b6b0b7ff28ad98ecd8675c00736
Edgar Muñoz <edmunoz@parpro.com>
2025-12-22 14:21:31 -0800

    add production environment variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost/metales/public/api',
+  csrfUrl: 'http://localhost/metales/public/'
+  //csrfUrl: 'http://parprohub'
+
+};
